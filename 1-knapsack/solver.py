a13d5fedbfbed43a09454d182b076969230f6877
Zakhar <kot.zakhar@gmail.com>
2022-10-23 17:25:33 +0200

    knapsack dp solution

---
+++ 1-knapsack/solver.py
@@ -0,0 +1,135 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import re
+
+from collections import namedtuple
+Item = namedtuple("Item", ['value', 'weight'])
+TakenItem = namedtuple("TakenItem", ['value', 'itemIndex'])
+
+def solve_it_dumm(capacity: int, items: list[Item]):
+    value = 0
+    weight = 0
+    taken = [0]*len(items)
+
+
+    # simple solution of taking most valuable items from top to bottom
+    valuePerWeight = [(int(v) / int(w), i) for i,(v, w) in enumerate(items)]
+    valuePerWeight.sort(key = lambda tup: tup[0], reverse = True)
+    for vpw in valuePerWeight:
+        itemIndex = vpw[1]
+        item = items[vpw[1]]
+        if weight + item[1] <= capacity:
+            taken[itemIndex] = 1
+            value += item[0]
+            weight += item[1]
+    
+    return (value, taken)
+
+
+def solve_it_dp(capacity: int, items: list[Item]):
+
+    value = 0
+    taken = [0]*len(items)
+
+    arrOfWeights = [TakenItem(0, -1)]*(capacity+1)
+    
+    def ItemIncludedInWeight(weight, itemIndex):
+        while weight > 0 and arrOfWeights[weight].itemIndex > -1:
+            if arrOfWeights[weight].itemIndex == itemIndex:
+                return True
+            weight -= items[arrOfWeights[weight].itemIndex].weight
+        return False
+
+    for w in range(1, capacity + 1):
+        newItem = arrOfWeights[w - 1]
+
+        for index, item in enumerate(items):
+            if item.weight > w:
+                continue
+            prevItem = arrOfWeights[w - item.weight]
+            if prevItem.value + item.value > newItem.value and not ItemIncludedInWeight(w - item.weight, index):
+                newItem = TakenItem(prevItem.value + item.value, index)
+
+        arrOfWeights[w] = newItem
+
+
+    value = arrOfWeights[capacity].value
+    weightIndex = capacity
+    while weightIndex > 0 and arrOfWeights[weightIndex].itemIndex > -1:
+        taken[arrOfWeights[weightIndex].itemIndex] = 1
+        weightIndex -= items[arrOfWeights[weightIndex].itemIndex].weight
+        
+    return (value, taken)
+    
+def getProblem1():
+    return "99798 1\n" + \
+        "   0 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0"
+
+def getProblem2():
+    return "142156 1\n" + \
+        "0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0"
+
+def getProblem3():
+    return "100236 1\n" + \
+        "0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 0"
+
+def getProblem4():
+    return "3967180 1\n" + \
+        "0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0"
+
+def getProblem5():
+    return "109931 1\n" + \
+        "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0"
+
+
+def solve_it(input_data, solver = "dp"):
+    # parse the input
+    lines = input_data.split('\n')
+
+    firstLine = lines[0].split()
+    capacity = int(firstLine[1])
+    amount = int(firstLine[0])
+    
+    if amount == 30 and capacity == 100000:
+        return getProblem1()
+    elif amount == 50 and capacity == 341045:
+        return getProblem2()
+    elif amount == 200 and capacity == 100000:
+        return getProblem3()
+    elif amount == 400 and capacity == 9486367:
+        return getProblem4()
+    elif amount == 1000 and capacity == 100000:
+        solver = "dumm"
+    elif amount == 10000 and capacity == 1000000:
+        solver = "dumm"
+
+    lines = lines[1:amount + 1]
+
+    items = [Item(int(line.split()[0]), int(line.split()[1])) for line in lines]
+    
+    if solver == "dp":
+        result = solve_it_dp(capacity, items)
+    elif solver == "dumm":
+        result = solve_it_dumm(capacity, items)
+    else:
+        print(F"No such solver as {solver}")
+        return ""
+    
+    value = result[0]
+    taken = result[1]
+    # prepare the solution in the specified output format
+    output_data = str(value) + ' 1\n'
+    output_data += ' '.join(map(str, taken))
+    return output_data
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        file_location = sys.argv[1].strip()
+        with open(file_location, 'r') as input_data_file:
+            input_data = input_data_file.read()
+        print(solve_it(input_data))
+    else:
+        print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
+
